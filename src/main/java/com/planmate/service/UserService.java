185b19b4d2c2d616f9f77e27303bb527e8434e59
gebeen-dev <rrdd1207@gmail.com>
2024-10-13 14:45:38 +0900

    add controller, service 추가

---
+++ src/main/java/com/planmate/service/UserService.java
@@ -0,0 +1,9 @@
+package com.planmate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+}
