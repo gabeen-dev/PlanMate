552575785db382c06b9f4776ff13d59f59bde1cf
gebeen-dev <rrdd1207@gmail.com>
2024-10-14 15:23:51 +0900

    update : usercontroller CRUD생성

--- src/main/java/com/planmate/controller/UserController.java
+++ src/main/java/com/planmate/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.planmate.controller;
 
+import com.planmate.model.dto.UserRequestDto;
+import com.planmate.model.dto.UserResponseDto;
 import com.planmate.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,4 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    //유저조회
+    public UserResponseDto searchUser(Long UserId) {
+        return null;
+    }
+
+    //유저생성
+    public UserResponseDto createUser(UserRequestDto userRequestDto) {
+        return null;
+    }
+
+    //유저수정
+    public UserResponseDto updateUser(UserRequestDto userRequestDto) {
+        return null;
+    }
+
+    //유저삭제
+    public void deleteUser(Long UserId) {
+
+    }
 }
