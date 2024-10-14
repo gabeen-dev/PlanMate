213b8a10f1117f6bfa51deec4af691b0c6e71c2e
gebeen-dev <rrdd1207@gmail.com>
2024-10-14 17:56:08 +0900

    update : entity-user 필드

--- src/main/java/com/planmate/model/entity/User.java
+++ src/main/java/com/planmate/model/entity/User.java
@@ -1,4 +1,33 @@
 package com.planmate.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userid;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String userMail;
+
+    @Column
+    private LocalDateTime createDate;
+
+    @Column
+    private LocalDateTime updateDate;
 }
