41181a96d428ca17e80fdb427c3882be8d65b1d7
gebeen-dev <rrdd1207@gmail.com>
2024-10-15 10:30:28 +0900

    update : CommentController CRUD

--- src/main/java/com/planmate/controller/CommentController.java
+++ src/main/java/com/planmate/controller/CommentController.java
@@ -1,5 +1,7 @@
 package com.planmate.controller;
 
+import com.planmate.model.dto.CommentRequestDto;
+import com.planmate.model.dto.CommentResponseDto;
 import com.planmate.service.CommentSercvice;
 import com.planmate.service.PlanService;
 import lombok.RequiredArgsConstructor;
@@ -10,4 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentController {
 
     private final CommentSercvice commentSercvice;
+
+    //TODO : comment CRUD
+    //댓글 조회
+    public CommentResponseDto getComment(Long commentId) {
+        return null;
+    }
+
+    //댓글 생성
+    public CommentResponseDto createComment(CommentRequestDto commentRequestDto) {
+        return null;
+    }
+
+    //댓글 수정
+    public CommentResponseDto updateComment(Long commentId, CommentRequestDto commentRequestDto) {
+        return null;
+    }
+
+    //댓글 삭제
+    public void deleteComment(Long commentId) {
+
+    }
 }
