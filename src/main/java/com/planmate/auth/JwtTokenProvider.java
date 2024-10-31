a13551cdee2e4c923c111ab0464b374d762b3685
gebeen-dev <rrdd1207@gmail.com>
2024-10-31 21:23:35 +0900

    update : JwtTokenProvider 추가

--- src/main/java/com/planmate/auth/JwtTokenProvider.java
+++ src/main/java/com/planmate/auth/JwtTokenProvider.java
@@ -4,9 +4,13 @@ import com.planmate.model.entity.Users;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Date;
 
@@ -34,6 +38,14 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+    public String getEmailFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
     public boolean validateToken(String token) {
         try {
             Jwts.parser()
@@ -46,6 +58,23 @@ public class JwtTokenProvider {
         }
     }
 
+    //JWT 추출- Authorization 헤더에서 Bearer 접두사를 제거하고 실제 JWT만 반환
+    public String extractJwtToken(String tokenWithBearer) {
+        if (tokenWithBearer != null && tokenWithBearer.startsWith("Bearer ")) {
+            return tokenWithBearer.substring(7);
+        }
+
+        return tokenWithBearer;
+    }
+
+    //현재 HTTP 요청 가져오기
+    private HttpServletRequest getCurrentHttpRequest() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes instanceof ServletRequestAttributes) {
+            return ((ServletRequestAttributes) requestAttributes).getRequest();
+        }
+        return null;
+    }
 
 
 }
