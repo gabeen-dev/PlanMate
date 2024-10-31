adb31c1647f598674c3cb06aa3092910188763ae
gebeen-dev <rrdd1207@gmail.com>
2024-10-31 21:19:43 +0900

    update : jwtTokenFilter 추가

--- src/main/java/com/planmate/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/planmate/auth/filter/JwtAuthenticationFilter.java
@@ -1,20 +1,46 @@
 package com.planmate.auth.filter;
 
+import com.planmate.auth.JwtTokenProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        if (requestURI.contains("/api")) {
+            String token = jwtTokenProvider.extractJwtToken(request.getHeader("Authorization"));
+            if (jwtTokenProvider.validateToken(token)) {
+                filterChain.doFilter(request, response);
+            } else {
+                extracted(response);
+            }
+        }
 
+        if (requestURI.contains("/auth")) {
+            filterChain.doFilter(request, response);
+        }
+
+    }
+
+    private void extracted(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 상태 코드 설정
+        response.setContentType("application/json"); // 응답 타입을 JSON으로 설정
+        response.setCharacterEncoding("UTF-8");
+        String jsonResponse = "{\"error\": \"토큰이 만료되었습니다.\"}";
+        response.getWriter().write(jsonResponse); // 응답 본문에 JSON 메시지 작성
     }
 
 }
